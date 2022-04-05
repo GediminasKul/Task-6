4e78840b3dc71e492ba10ca921380ae0c6de0b7e
GediminasKul <gediminas.kulakauskas@gmail.com>
2022-04-05 08:09:12 +0300

    Prideta getUsersNames funkcija, kuri paima is masyvo objektu vardus ir ikelia i nauja masyva

--- script.js
+++ script.js
@@ -19,3 +19,16 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+// 2
+
+function getUsersNames(users) {
+  const solution = [];
+
+  for (let i = 0, length = users.length; i < length; i++) {
+    solution.push(users[i].name);
+  }
+  return solution;
+}
+
+console.log(getUsersNames(users));
